3b00684ce4cfca6787531acf9c23e7e2988bf3b6
Joon-Mo Yang <jmyang@kodebox.io>
2018-07-06 20:02:49 +0900

    Add skeleton code using hyper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate hyper;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn;
+
+type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
+fn get_work(_req: Request<Body>) -> BoxFut {
+    unimplemented!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // FIXME: Get notification address from command line option
+    const RECEIVE_PORT: u16 = 3333;
+    let addr = ([127, 0, 0, 1], RECEIVE_PORT).into();
+
+    let server = Server::bind(&addr)
+        .serve(|| service_fn(get_work))
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    hyper::rt::run(server);
 }
