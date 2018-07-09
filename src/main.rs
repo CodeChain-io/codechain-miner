4506765cb6e3363c27e6902d9a534d54e4c83208
Joon-Mo Yang <jmyang@kodebox.io>
2018-07-09 18:34:11 +0900

    Implement cuckoo worker

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate byteorder;
+extern crate crypto;
+extern crate cuckoo;
 extern crate ethereum_types;
 extern crate env_logger;
 extern crate futures;
 extern crate hyper;
 #[macro_use]
 extern crate log;
+extern crate rlp;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
--- src/worker/cuckoo.rs
+++ src/worker/cuckoo.rs
@@ -1,30 +1,71 @@
-use ethereum_types::U256;
+use byteorder::{ByteOrder, LittleEndian};
+use crypto::blake2b::Blake2b;
+use crypto::digest::Digest;
+use cuckoo::Cuckoo;
+use ethereum_types::{H256, U256};
+use rlp;
 
 use super::Worker;
 
+pub fn blake256<T: AsRef<[u8]>>(s: T) -> H256 {
+    let input = s.as_ref();
+    let mut result = H256::default();
+    let mut hasher = Blake2b::new(32);
+    hasher.input(input);
+    hasher.result(&mut *result);
+    result
+}
+
 pub struct CuckooWorker {
+    message: Vec<u8>,
+    nonce: u64,
+    target: U256,
+    is_executed: bool,
+    solver: Cuckoo,
 }
 
 impl CuckooWorker {
     pub fn new() -> Self {
         Self {
+            message: Vec::new(),
+            nonce: 0,
+            target: U256::zero(),
+            is_executed: false,
+            // FIXME: get parameters from command line option
+            solver: Cuckoo::new(16, 8, 4),
         }
     }
 }
 
 impl Worker for CuckooWorker {
     fn init(&mut self,  message: &[u8], nonce: u64, target: &U256) {
-        unimplemented!()
+        self.message = message.to_vec();
+        // FIXME: check message length
+        LittleEndian::write_u64(&mut self.message, nonce);
+
+        self.nonce = nonce;
+        self.target = *target;
     }
 
     fn proceed(&mut self) -> Option<Vec<Vec<u8>>> {
         if self.is_finished() {
             return None
         }
-        unimplemented!()
+        self.is_executed = true;
+        if let Some(proof) = self.solver.solve(&self.message) {
+            let hash = blake256(rlp::encode_list(&proof));
+            if U256::from(hash) <= self.target {
+                info!("Solution found: {:?}", proof);
+                let nonce_bytes = ::rlp::encode(&self.nonce).to_vec();
+                let proof_bytes = ::rlp::encode_list(&proof).to_vec();
+
+                return Some(vec![nonce_bytes, proof_bytes])
+            }
+        }
+        None
     }
 
     fn is_finished(&self) -> bool {
-        unimplemented!()
+        self.is_executed
     }
 }
